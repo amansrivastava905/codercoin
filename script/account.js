ea772c11fa82692d34a67fe0e88874846c9d9d85
Aman Srivastava <amansrivastava905@gmail.com>
2020-08-05 12:20:02 +0530

    account page revised.

--- script/account.js
+++ script/account.js
@@ -1,14 +1,27 @@
-const passBtn=document.querySelector(".password");
-passBtn.addEventListener("click",makeVissible);
+let streakValue=document.querySelector(".winning-streak .value").textContent;
+let streak=document.querySelector(".winning-streak .value");
+
+function streakbar(e){
+    streak.style.backgroundSize=e;
+}
+
+streakbar(streakValue);
+console.log(streak.style.backgroundSize);
 
 
-function makeVissible(){
-   div=document.querySelector(".password-setting");
-   if(div.style.display=="none")
-   {
-       div.style.display="block";
-   }
-   else{
-       div.style.display="none";
-   }
+
+let settingDiv=document.querySelector(".settings");
+let settingBtn=document.querySelector(".buttons .setting-button");
+settingBtn.addEventListener("click",settingVis);
+
+function settingVis(){
+    if(settingDiv.style.display=="none")
+    {
+        settingDiv.style.display="block";
+        document.getElementById('coverAll2').style.display = 'block';
+    }
+    else{
+        settingDiv.style.display="none";
+        document.getElementById('coverAll2').style.display = 'none';
+    }
 }
